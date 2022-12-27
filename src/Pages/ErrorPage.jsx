3d7687f52f26793affb8267f34e9aafd1c1888ec
Hisham khaled <hishamk1999@gmail.com>
2022-12-27 22:43:48 +0200

    Add button in error page navigate to home.

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { useRouteError } from "react-router-dom";
+import { useRouteError, useNavigate } from "react-router-dom";
 
 function ErrorPage() {
 	const error = useRouteError();
+	const navigate = useNavigate();
 	console.error(error);
 	return (
 		<div className="errorPage container">
-			<div id="error-page">
+			<div id="error-page" className=" mt-5 text-center">
 				<h1>Oops!</h1>
 				<p>Sorry, an unexpected error has occurred.</p>
 				<p>
 					<i>{error.statusText || error.message}</i>
 				</p>
+				<button
+					type="button"
+					className="btn btn-primary"
+					onClick={() => navigate("/",{replace: true})}
+				>
+					Back to Home
+				</button>
 			</div>
 		</div>
 	);
